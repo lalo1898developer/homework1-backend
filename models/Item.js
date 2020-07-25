75d8b677d962ac0693ef44447acac1da0fb11fdd
Eduardo Rodriguez <lalo.work.tech@gmail.com>
2020-07-25 15:46:59 -0500

    Crea el modelo de Item con solo 2 objetos para probar

---
+++ models/Item.js
@@ -0,0 +1,16 @@
+const Item = [
+    {
+        id: 1,
+        name: "Refresco",
+        price: 20,
+        quantity: 10
+    },
+    {
+        id: 2,
+        name: "Papas",
+        price: 20,
+        quantity: 5
+    },
+];
+
+module.exports = Item;
