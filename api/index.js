e6cf7558f8df8e7725cebaaf5a27fc6b810b1596
Eduardo Rodriguez <lalo.work.tech@gmail.com>
2020-07-25 15:50:27 -0500

    Añade el middleware de las rutas que fueron creadas

--- api/index.js
+++ api/index.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 3000;
 api.use(morgan('dev'));
 api.use(express.urlencoded({ extended: true }));
 api.use(express.json({ express: true }));
+api.use('/api/', require('../routers'));
 
 api.get('/', (req, res) => res.send('Esta viva y funcionando!'));
 
