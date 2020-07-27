94a97ad6da9998077cbe1053f60ccbc1bb69c734
Eduardo Rodriguez <lalo.work.tech@gmail.com>
2020-07-26 21:35:24 -0500

    Usa como middleware a CORS para habilitar las peticiones

--- api/index.js
+++ api/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 
 const api = express();
 const PORT = process.env.PORT || 3000;
 
+api.use(cors());
 api.use(morgan('dev'));
 api.use(express.urlencoded({ extended: true }));
 api.use(express.json({ express: true }));
