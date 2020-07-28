c59f754186035628e2b851fa2e0523b2e50a44dc
Eduardo Rodriguez <lalo.work.tech@gmail.com>
2020-07-28 13:29:09 -0500

    Añade 18 objetos más

--- models/Item.js
+++ models/Item.js
@@ -2,14 +2,122 @@ const Item = [
     {
         id: 1,
         name: "Refresco",
-        price: 20,
-        quantity: 10
+        price: 15,
+        quantity: 12
     },
     {
         id: 2,
         name: "Papas",
-        price: 20,
+        price: 22,
         quantity: 5
+    },
+    {
+        id: 3,
+        name: "Galletas",
+        price: 12,
+        quantity: 21
+    },
+    {
+        id: 4,
+        name: "Leche",
+        price: 25,
+        quantity: 10
+    },
+    {
+        id: 5,
+        name: "Jamón",
+        price: 35,
+        quantity: 1
+    },
+    {
+        id: 6,
+        name: "Queso",
+        price: 40,
+        quantity: 2
+    },
+    {
+        id: 7,
+        name: "Palomitas",
+        price: 17,
+        quantity: 20
+    },
+    {
+        id: 8,
+        name: "Mantequilla",
+        price: 28,
+        quantity: 12
+    },
+    {
+        id: 9,
+        name: "Café",
+        price: 40,
+        quantity: 3
+    },
+    {
+        id: 10,
+        name: "Jugos",
+        price: 23,
+        quantity: 7
+    },
+    {
+        id: 11,
+        name: "Yogurth",
+        price: 12,
+        quantity: 21
+    },
+    {
+        id: 12,
+        name: "Natilla",
+        price: 7,
+        quantity: 20
+    },
+    {
+        id: 13,
+        name: "Cigarros",
+        price: 50,
+        quantity: 10
+    },
+    {
+        id: 14,
+        name: "Tang",
+        price: 3,
+        quantity: 20
+    },
+    {
+        id: 15,
+        name: "Pan Bimbo",
+        price: 25,
+        quantity: 13
+    },
+    {
+        id: 16,
+        name: "Huevo",
+        price: 35,
+        quantity: 70
+    },
+    {
+        id: 17,
+        name: "Chiles",
+        price: 10,
+        quantity: 22
+    },
+    {
+        id: 18,
+        name: "Catsup",
+        price: 30,
+        quantity: 19
+    },
+    {
+        id: 19,
+        name: "Azucar",
+        price: 33,
+        quantity: 10
+    },
+    {
+        id: 20,
+        name: "Frijol",
+        price: 10,
+        quantity: 6
     },
 ];
 
