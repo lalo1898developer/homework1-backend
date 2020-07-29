76a8eec6b98f345d81bcab5176aeffbb7d6c3bd2
Eduardo Rodriguez <lalo.work.tech@gmail.com>
2020-07-28 21:33:04 -0500

    Añade comentaios para la documentación de Swagger

--- routers/ItemRouter.js
+++ routers/ItemRouter.js
@@ -4,8 +4,43 @@ const router = express.Router();
 
 const { ItemController } = require('../controllers');
 
+/**
+ * @swagger
+ * /api/items:
+ *  get:
+ *   description: Regresa un Array con todos los items como objeto
+ *   responses:
+ *    '200':
+ *      description: Petición Exitosa
+ *    '400':
+ *      description: Error en la petición
+ */
 router.get('/items/', ItemController.readAll);
+
+/**
+ * @swagger
+ * /api/items/total:
+ *  get:
+ *   description: Regresa un número, el cual representa la suma de todos los valores de la llave "quantity" de los items
+ *   responses:
+ *    '200':
+ *      description: Petición Exitosa
+ *    '400':
+ *      description: Error en la petición
+ */
 router.get('/items/total', ItemController.readTotal);
+
+/**
+ * @swagger
+ * /api/items/average:
+ *  get:
+ *   description: Regresa un número, el cual representa el promedio de todos los valores de la llave "price" de los items
+ *   responses:
+ *    '200':
+ *      description: Petición Exitosa
+ *    '400':
+ *      description: Error en la petición
+ */
 router.get('/items/average', ItemController.readAverage);
 
 module.exports = router;
