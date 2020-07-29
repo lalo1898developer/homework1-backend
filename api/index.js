4d53dbf992b232e8db8a77112bd410429b2e3dbc
Eduardo Rodriguez <lalo.work.tech@gmail.com>
2020-07-28 21:32:40 -0500

    Añade configuración de Swagger

--- api/index.js
+++ api/index.js
@@ -1,15 +1,35 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 
 const api = express();
 const PORT = process.env.PORT || 3000;
+
+const swaggerOptions = {
+    swaggerDefinition: {
+        info: {
+            title: 'Homework1-Backend API',
+            description: 'Es una de 2 tareas para buscar la oportunidad de entrar a un proyecto con Raiffeisen Latina',
+            contact: {
+                name: 'Eduardo Rodriguez Ricardez'
+            },
+            servers: ['https://homework1-backend.herokuapp.com/', 'http://localhost:3000']
+        }
+    },
+    apis: ['./routers/ItemRouter.js']
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+
 
 api.use(cors());
 api.use(morgan('dev'));
 api.use(express.urlencoded({ extended: true }));
 api.use(express.json({ express: true }));
 api.use('/api/', require('../routers'));
+api.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 api.get('/', (req, res) => res.send('Esta viva y funcionando!'));
 
